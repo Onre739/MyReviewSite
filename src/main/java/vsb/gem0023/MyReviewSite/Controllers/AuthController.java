ba1c649122bda310063c834b1ad52ce5cd8bc3d8
Onre739 <ondragemrot@seznam.cz>
2025-04-12 15:04:11 +0200

    logged username + make rev btn

--- src/main/java/vsb/gem0023/MyReviewSite/Controllers/AuthController.java
+++ src/main/java/vsb/gem0023/MyReviewSite/Controllers/AuthController.java
@@ -1,5 +1,6 @@
 package vsb.gem0023.MyReviewSite.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -8,6 +9,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vsb.gem0023.MyReviewSite.Entities.User;
+import vsb.gem0023.MyReviewSite.Repositories.UserRepository;
+import vsb.gem0023.MyReviewSite.Services.MyUserDetailsService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +20,13 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public AuthController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/check")
     public ResponseEntity<Map<String, Object>> checkAuthStatus() {
@@ -33,6 +44,15 @@ public class AuthController {
             response.put("roles", auth.getAuthorities().stream()
                     .map(GrantedAuthority::getAuthority)
                     .collect(Collectors.toList()));
+
+            User user = userRepository.findByEmail(auth.getName())
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            response.put("userDetails", Map.of(
+                    "fullName", user.getName(),
+                    "email", user.getEmail(),
+                    "avatarUrl", user.getRole()
+            ));
         }
 
         return ResponseEntity.ok(response);
